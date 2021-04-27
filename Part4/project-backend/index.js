4c1d21a50145aa1e78074d628ce113edf75dbbca
aventusm <8antonm@gmail.com>
2021-04-27 22:36:46 +0300

    4.1 and 4.2 Done. This is an intermediate commit as there were some project reconfigurations after GKE being made

---
+++ Part4/project-backend/index.js
@@ -0,0 +1,86 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  port: 5432,
+});
+const TABLE_NAME = 'todos';
+
+pool.once('connect', async (client) => {
+  const tableCreateQuery = `CREATE TABLE IF NOT EXISTS ${TABLE_NAME} (id SERIAL PRIMARY KEY, text VARCHAR(140))`;
+  const valueInsertQuery = `INSERT INTO ${TABLE_NAME} VALUES (DEFAULT, 'Part 4 todo value')`;
+
+  try {
+    const creationResult = await client.query(tableCreateQuery);
+    console.log('creationResult', creationResult);
+    const insertionResult = await client.query(valueInsertQuery);
+    console.log('insertionResult', insertionResult);
+  } catch (error) {
+    console.log('db error connect');
+  }
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use((req, _res, next) => {
+  console.log('--- BACKEND/TODOS REQUEST---');
+  console.log(`Method ${req.method} /// Path ${req.path}`);
+  console.log('BODY', req.body);
+  next();
+});
+
+app.get('/', async (_req, res) => {
+  try {
+    const connection = await pool.connect();
+    const result = await connection.query(`SELECT * FROM ${TABLE_NAME}`);
+    connection.release();
+    res.send({ list: result.rows });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.post('/', async (req, res) => {
+  try {
+    if (`${req.body.todo}`.length > 140) {
+      const errorMessage = 'Todo length is over 140 characters';
+      console.log(errorMessage);
+      res.status(401).end(errorMessage);
+    } else {
+      const connection = await pool.connect();
+      await connection.query(
+        `INSERT INTO ${TABLE_NAME} VALUES (DEFAULT, '${req.body.todo}')`,
+      );
+      connection.release();
+      res.sendStatus(200);
+    }
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+// Ready when path can establish a connection to the database
+app.get('/healthz', async (_req, res) => {
+  try {
+    const connection = await pool.connect();
+    const result = await connection.query(`SELECT * FROM ${TABLE_NAME}`);
+    connection.release();
+    res.status(200).send({ rowCount: result.rowCount });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+const PORT = 3002;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} (project-backend)`);
+});
---
+++ Part4/split-getting-started/timestamp-response/index.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+const directory = path.join('files');
+const logFileName = 'log.txt';
+const timestampHashLogFilePath = path.join(directory, logFileName);
+const randomInitHash = Math.random().toString(36).substring(3, 8);
+const ENV_MESSAGE = process.env.MESSAGE || 'Message variable not applied';
+
+// Copied from app3/images-response
+const getFileContents = async (paramFilePath) =>
+  new Promise((res) => {
+    fs.readFile(paramFilePath, (err, buffer) => {
+      if (err) {
+        console.log('error:::', err);
+        res(false);
+      } else {
+        res(buffer.toString());
+      }
+    });
+  });
+
+app.get('/', async (_req, res) => {
+  try {
+    const timestampData = await getFileContents(timestampHashLogFilePath);
+    const pingPongDataResponse = await axios.get(
+      'http://ping-pong-service.course-exercises:2347',
+    ); // Should get the current value without increasing it
+
+    const renderedMessage = `<p>${ENV_MESSAGE}</p>`;
+    const hashTimestamp = `<p>${timestampData} ${randomInitHash}</p>`;
+    const responseCounter = `<p>Pings / Pongs: ${pingPongDataResponse.data.counter}</p>`;
+    res
+      .status(200)
+      .send(`${renderedMessage} ${hashTimestamp} ${responseCounter}`);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+// Ready when it can receive data from pingpong application
+app.get('/healthz', async (_req, res) => {
+  try {
+    const pingPongDataResponse = await axios.get(
+      'http://ping-pong-service.course-exercises:2347',
+    );
+    res.status(200).send({ data: pingPongDataResponse.data });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} (timestamp-response)`);
+});
